c2c364e6e6a46bc89a4129108052bdb5e2294411
= <daniender@gmail.com>
2021-03-30 13:56:12 +0200

    Initial commit

---
+++ src/main/java/com/daniender/demo/zuul/DockerApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.daniender.demo.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class DockerApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniender/demo/zuul/DockerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniender.demo.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
